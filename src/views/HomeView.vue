73955a7655068492828e6d3c1e7458cdf00de7e8
charlesnyanje <charleswilliam26@gmail.com>
2023-03-15 10:47:10 +0300

    added home router

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from "../components/TheWelcome.vue";
+import FetchApi from "../components/FetchApi.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <FetchApi />
   </main>
 </template>
